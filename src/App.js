8e1d98625cd430552da18e8852e57bd939435bec
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-10 12:54:36 +0200

    removed wrong input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-	render() {
-		return <div className="App" />;
-	}
+  render() {
+    return <div className="App" />;
+  }
 }
 
 export default App;
