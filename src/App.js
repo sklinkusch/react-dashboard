d2e92971bee8f27c83d6d68dbacceece97d7fb12
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-10 12:55:42 +0200

    imported background file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import Background from "./Components/Background";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Background />
+      </div>
+    );
   }
 }
 
