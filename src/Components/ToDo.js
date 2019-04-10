c08eb85ab76cb35926a9add28bc5a29a4c4b55f3
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-10 14:56:43 +0200

    todo done w/o localStorage

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -7,11 +7,20 @@ export default class ToDo extends Component {
       todo: []
     }
   }
+  handleSubmit(event) {
+    if (event.key === 'Enter') {
+      const newItem = event.target.value;
+      const allItems = this.state.todo;
+      const mergedItems = allItems.concat(newItem);
+      this.setState({ todo: mergedItems });
+      event.target.value = "";
+    }
+  }
   render() {
     return (
       <div className="todo">
         <h2>What is your plan for today?</h2>
-        <input type="text" />
+        <input type="text" onKeyPress={event => this.handleSubmit(event)} />
         <div className="todo-list">
           <ul>
             {this.state.todo.map((item, index) => (
