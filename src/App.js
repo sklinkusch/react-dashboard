fdc1186619d4fc4fad66ecc5b03c85f617bfa421
Stefan Wurck <vitatech@gmx.de>
2019-04-10 11:49:12 +0200

    created files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-            </div>
-    );
-  }
+	render() {
+		return <div className="App" />;
+	}
 }
 
 export default App;
