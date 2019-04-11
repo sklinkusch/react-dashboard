88989d417ff65dfc09b10a9c8b532e44a5dacf77
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-11 10:29:38 +0200

    changed text in link

--- src/Components/Credits.js
+++ src/Components/Credits.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Credits() {
   return (
     <div className="credentials">
-      <a href="https://www.shareicon.net/multimedia-arrows-reload-refresh-orientation-direction-multimedia-option-808940" target="_blank" rel="noopener noreferrer">
-        https://www.shareicon.net/multimedia-arrows-reload-refresh-orientation-direction-multimedia-option-808940
+      <a href="https://www.shareicon.net/multimedia-arrows-reload-refresh-orientation-direction-multimedia-option-808940" target="_blank" rel="noopener noreferrer" title="Flaticon Basic License">
+        Source: ShareIcon
       </a>
     </div>
   )
