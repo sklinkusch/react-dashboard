7765eade5634095617bea1631e7d2b09aa84dad7
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-11 10:29:25 +0200

    created function with link to source

--- src/Components/Credits.js
+++ src/Components/Credits.js
@@ -1,1 +1,13 @@
-//https://www.shareicon.net/multimedia-arrows-reload-refresh-orientation-direction-multimedia-option-808940
+import React from 'react'
+
+export default function Credits() {
+  return (
+    <div className="credentials">
+      <a href="https://www.shareicon.net/multimedia-arrows-reload-refresh-orientation-direction-multimedia-option-808940" target="_blank" rel="noopener noreferrer">
+        https://www.shareicon.net/multimedia-arrows-reload-refresh-orientation-direction-multimedia-option-808940
+      </a>
+    </div>
+  )
+}
+
+//
