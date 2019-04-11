99dabea04ee19dcb60272d0fb25d860305470225
Stefan Wurck <vitatech@gmx.de>
2019-04-11 10:57:28 +0200

    Updates

---
+++ precache-manifest.19c90e1a2a43342d2261f7592bbdc08e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "582ccc7f229222f52ec6",
+    "url": "/react-dashboard/static/js/runtime~main.6bc13b0f.js"
+  },
+  {
+    "revision": "fe504bca6c3e54bf6130",
+    "url": "/react-dashboard/static/js/main.86bf1c2b.chunk.js"
+  },
+  {
+    "revision": "5c4dbae84c8b38fc8d33",
+    "url": "/react-dashboard/static/js/2.4b50650a.chunk.js"
+  },
+  {
+    "revision": "fe504bca6c3e54bf6130",
+    "url": "/react-dashboard/static/css/main.690073c7.chunk.css"
+  },
+  {
+    "revision": "9ef29a6d36c60747fa912862c6b9c497",
+    "url": "/react-dashboard/index.html"
+  }
+];
